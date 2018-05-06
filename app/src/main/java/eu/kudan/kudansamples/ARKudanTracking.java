121e0adbc0d634cec2df40010b7165dec99cb479
Lưu Trung <luuvantrung102@gmail.com>
2018-05-06 19:20:45 +0900

    Add app's source
    Add app's source

---
+++ app/src/main/java/eu/kudan/kudansamples/ARKudanTracking.java
@@ -0,0 +1,16 @@
+package eu.kudan.kudansamples;
+
+import android.os.Bundle;
+
+import eu.kudan.kudan.ARActivity;
+
+public class ARKudanTracking extends ARActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView(R.layout.activity_arcamera);
+    }
+
+
+}
